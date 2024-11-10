a157abea4da51f2cb98d0c9456af82b3d6b56e5f
Salhi Med Houssem <salhimedhoussem@gmail.com>
2024-11-10 15:33:24 +0100

    import dataset

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("hefdfy")
+import numpy as np
+import pandas as pd
+medical_df = pd.read_csv('https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv')
+print(medical_df)
+medical_df.info()
